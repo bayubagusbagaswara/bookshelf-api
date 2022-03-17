d5482268932a217ab7d2b186498859bc363c0f72
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-17 07:45:48 +0700

    update path route

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ const { addBookHandler, getBookByIdHandler } = require('./handler');
 const routes = [
   {
     method: 'POST',
-    path: '/notes',
+    path: '/books',
     handler: addBookHandler,
   },
   {
     method: 'GET',
-    path: '/notes/{id}',
+    path: '/books/{id}',
     handler: getBookByIdHandler,
   },
 ];
